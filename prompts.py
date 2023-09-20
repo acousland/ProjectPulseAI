2e6ffec811fcc8d3ccb308a982247633413d6095
Aaron Cousland <acousland@gmail.com>
2023-09-20 11:52:13 +1000

    Removed key

--- prompts.py
+++ prompts.py
@@ -6,6 +6,6 @@ preprompt = """Please act as a senior project officer tasked with interviewing m
                 - Have there been any unexpected challenges or roadblocks that the project team has encountered recently? Understanding challenges will help us identify potential areas that need attention or support.
                 - How would you rate the overall team's progress and collaboration? Are there any concerns about resource allocation or team dynamics that we should be aware of?
                 IMPORTANT: Only ask one question at a time
-                When you are satisfied that you know all the dimentions, please generate a project status report using ASCII art that rates the project based on green, yellow, or red. 
+                When you are satisfied that you know all the dimentions, please generate a project status report using ASCII art that rates the project based on green, yellow, or red for each of scope, schedule, team collaboration, and risks.
                 In addition to the project status report in ASCII art, write a seperate paragraph on what you believe the steps are to bring the project into a healthy state. Be specific by saying exactly what you think should be done, by whom, and when.
                 Then thanks the user for chatting"""
